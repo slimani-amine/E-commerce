5a0564e3883271114cb118e391a67cdbb0603958
slimani-amine <slimanimedamine24@gmail.com>
2023-09-11 17:02:44 +0100

    some fixsation

--- myapp/components/User.tsx
+++ myapp/components/User.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import userOff from "../public/userOff.svg";
 import userOn from "../public/userOn.svg";
+import { useState } from "react";
 export const User = ({
   override,
   user,
@@ -8,14 +9,16 @@ export const User = ({
   override?: React.CSSProperties;
   user: string;
 }) => {
+  const[href,setHref]=useState("/logIn")
   let userSrc;
   if (user === "Off") {
     userSrc = userOff;
-  } else {
+  } else if(user==="on"){
     userSrc = userOn;
+    setHref("/accout")
   }
-  return (<Link href="/cart"> 
-  <img className="w-8 h-8" src={userSrc.src} style={override} />
+  return (<Link href={href}> 
+  <img className="w-8 h-8" src={userSrc} style={override} />
   </Link>
   );
 };
