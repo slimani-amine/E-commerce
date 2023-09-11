89d001437243580b75e3e1e70a805ca3557ae77f
haydercharfeddin <haydercharfeddin51@gmail.com>
2023-09-11 17:07:24 +0100

    done

--- myapp/components/Login.tsx
+++ myapp/components/Login.tsx
@@ -1,5 +1,5 @@
 "use_client";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Footer } from "./Footer";
 import { Line } from "../public/line";
 import { useRouter } from "next/router";
@@ -45,7 +45,6 @@ export const Login = ({ override }: { override?: React.CSSProperties }) => {
       setError('Invalid email or password');
     }
   };
-
 
 
 
