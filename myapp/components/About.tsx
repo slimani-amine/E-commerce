ea3a5ef79dd1cfd8648fb9768d38e825b68b6cff
haydercharfeddin <haydercharfeddin51@gmail.com>
2023-09-12 11:23:17 +0100

    wissem

--- myapp/components/About.tsx
+++ myapp/components/About.tsx
@@ -1,12 +1,53 @@
-import React from "react";
 
+import React from "react";
 export const About = ({ override }: { override?: React.CSSProperties }) => {
   return (
     <div
-      className="relative w-full h-[1533px] bg-white font-[Poppins] "
+      className="relative w-full h-[2033px] bg-white font-[Poppins] "
       style={override}
     >
-      <div>about</div>
+      {/* <TopHeader
+        override={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+        }}
+      />
+      <HeaderComponent
+        override={{
+          position: "absolute",
+          top: "88px",
+          left: "135px",
+        }}
+      />
+      <Line_3
+        override={{
+          position: "absolute",
+          top: "142px",
+          left: "0px",
+        }}
+      />
+      <Roadmap
+        override={{
+          position: "absolute",
+          top: "222px",
+          left: "135px",
+        }}
+      /> */}
+        <div className="mt-[350px] ml-[350px]">
+          <div>
+            <img src="https://www.haydengirls.com/cdn/shop/products/GY6111-LIGHT_20DENIM-1_941830b3-8c63-4f16-a89d-76d15d3913d3_800x.jpg?v=1693034274
+      " alt="dfdfdf" className="w-[700px] h-[800px] mt-[10px] ml-[900px]" />
+          </div>
+            <h1 className="text-3xl font-bold mt-[-550px]">Our Story</h1><div>
+            <h2 className="mt-[50px] text-xl">Launced in 2015, Exclusive is South Asia’s premier online shopping <br /> makterplace with an active presense in Bangladesh. <br /> Supported by wide range of tailored marketing, <br /> data and service solutions, Exclusive has 10,500 sallers and 300 brands <br /> and serves 3 millioons customers across the region. </h2><br />
+            Exclusive has more than 1 Million products to offer, growing at a very fast <br /> Exclusive offers a diverse assotment in categories ranging  from consumer.
+            </div>
+        </div><div className="rounded-full h-[80px] w-[80px] mt-[700px] ml-[400px] border-4 bg-black"></div>
+        <div className="h-[300px] w-[300px] border-2 ml-[300px] mt-[-200px]"><img src="https://www.iconpacks.net/icons/2/free-store-icon-1977-thumb.png" className="bg-white w-[55px] h-[55px] ml-[112px] mt-[132px] rounded-full" alt="" /></div>
+        <div className="h-[300px] w-[300px] border-2 ml-[700px] mt-[-300px]"></div>
+        <div className="h-[300px] w-[300px] border-2 ml-[1100px] mt-[-300px]"></div>
+        <div className="h-[300px] w-[300px] border-2 ml-[1500px] mt-[-300px]"></div>
     </div>
   );
 };
--- myapp/components/Cart.tsx
+++ myapp/components/Cart.tsx
@@ -1,10 +1,76 @@
+// export const Cart = ({ override }: { override?: React.CSSProperties }) => {
+//   return (
+//     <div
+//       className="relative w-full h-[1533px] bg-white font-[Poppins] "
+//       style={override}
+//     >
+//       <div>Cart</div>
+//     </div>
+//   );
+// };
 export const Cart = ({ override }: { override?: React.CSSProperties }) => {
   return (
     <div
       className="relative w-full h-[1533px] bg-white font-[Poppins] "
       style={override}
     >
-      <div>Cart</div>
+      {/* <TopHeader
+        override={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+        }}
+      /> */}
+      {/* <HeaderComponent
+        override={{
+          position: "absolute",
+          top: "88px",
+          left: "135px",
+        }}
+      />
+      <Line_3
+        override={{
+          position: "absolute",
+          top: "142px",
+          left: "0px",
+        }}
+      />
+      <Roadmap
+        override={{
+          position: "absolute",
+          top: "222px",
+          left: "135px",
+        }}
+      /> */}
+<div>
+            <div className="ml-[180px] mt-[300px] w-[1650px] h-[60px] border-2 rounded">
+              <h1 className="text-xl  mt-[13px] ml-[50px]">Products</h1>
+              <h1 className="text-xl  ml-[560px] mt-[-26px]">Price</h1>
+              <h1 className="text-xl  ml-[950px] mt-[-26px]">Quantity</h1>
+              <h1 className="text-xl  ml-[1300px] mt-[-26px]">Subtotal</h1>
+            </div>
+            <div>
+              <div className="ml-[180px] mt-[100px] w-[1650px] h-[60px] border-2 rounded">
+<img src="product" alt="productPhoto" className="h-[40px] w-[80px]" />
+<h1 className="ml-[570px] mt-[-18px]">Price</h1>
+<div className=" ml-[965px] mt-[-40px] h-[40px] w-[70px] border-2">1</div>
+              </div>
+            </div>
+        </div>
+      {/* <Footer
+        override={{
+          position: "absolute",
+          top: "1093px",
+          left: "0px",
+        }}
+      />
+      <div>Cart</div> */}
     </div>
   );
 };
+
+
+
+
+
+
